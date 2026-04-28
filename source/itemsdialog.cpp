087c0b0f69dea21988ac73cc4e7033ca5ca6c110
pionere <pionere@freemail.hu>
2026-04-28 11:22:39 +0200

    is 18t

--- source/itemsdialog.cpp
+++ source/itemsdialog.cpp
@@ -962,7 +962,7 @@ void ItemsDialog::on_calculateButton_clicked()
     auto gameMulti = IsMultiGame;
     IsMultiGame = this->ui->isMultiCheckBox->isChecked();
 
-    float dropChance = this->ui->itemSourceComboBox->currentIndex() == 0 ? ItemDropChance(idx, lvl, this->numPlayers, unique) : (.0f / .0f);
+    float dropChance = this->ui->itemSourceComboBox->currentIndex() == 0 ? ItemDropChance(idx, lvl, this->numPlayers, unique) : NAN;
 
     IsMultiGame = gameMulti;
     IsHellfireGame = gameHellfire;
