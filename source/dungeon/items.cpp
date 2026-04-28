0efd2f0be60c7a0b3c9cd074a7c8a84825f6e65c
pionere <pionere@freemail.hu>
2026-04-28 12:17:27 +0200

    is 19

--- source/dungeon/items.cpp
+++ source/dungeon/items.cpp
@@ -2141,6 +2141,54 @@ void RecreateItem(int32_t iseed, uint16_t wIndex, uint16_t wCI)
     snprintf(items[MAXITEMS]._iName, sizeof(items[MAXITEMS]._iName), "%s", ItemName(&items[MAXITEMS]));
 }
 
+int GetItemBonusFlags(int itype, int misc_id)
+{
+    int flgs = 0;
+    switch (itype) {
+    case ITYPE_MISC:
+        if (misc_id != IMISC_MAP) {
+            if (misc_id == IMISC_BOOK || misc_id == IMISC_SCROLL || misc_id == IMISC_RUNE)
+                flgs = PLT_MISC;
+            break;
+        }
+        flgs = PLT_MAP;
+        break;
+    case ITYPE_SWORD:
+    case ITYPE_AXE:
+    case ITYPE_MACE:
+        flgs = PLT_MELEE;
+        break;
+    case ITYPE_BOW:
+        flgs = PLT_BOW;
+        break;
+    case ITYPE_SHIELD:
+        flgs = PLT_SHLD;
+        break;
+    case ITYPE_LARMOR:
+        flgs = PLT_ARMO | PLT_LARMOR;
+        break;
+    case ITYPE_HELM:
+        flgs = PLT_ARMO;
+        break;
+    case ITYPE_MARMOR:
+        flgs = PLT_ARMO | PLT_MARMOR;
+        break;
+    case ITYPE_HARMOR:
+        flgs = PLT_ARMO | PLT_HARMOR;
+        break;
+    case ITYPE_STAFF:
+        flgs = PLT_STAFF | PLT_CHRG;
+        break;
+    case ITYPE_GOLD:
+        break;
+    case ITYPE_RING:
+    case ITYPE_AMULET:
+        flgs = PLT_JEWEL;
+        break;
+    }
+    return flgs;
+}
+
 float ItemDropChance(int wIndex, int lvl, int numPlayers, bool uniqueMonster)
 {
     int quality = CFDQ_UNIQUE;
